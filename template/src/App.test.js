c9beb601aa7298d50444f222b4d436662f55d6bc
Revath S Kumar <rsk@revathskumar.com>
2020-02-03 19:53:24 +0530

    Add some example tests

--- template/src/App.test.js
+++ template/src/App.test.js
@@ -1,9 +1,47 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, within, fireEvent } from "../tests/utils";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders home", () => {
+  const { container } = render(<App />, {
+    initialEntries: ["/"],
+    initialIndex: 0,
+  });
+  const { getByText } = within(container.querySelector(".section"));
+  const home = getByText(/Home/i);
+  expect(home).toBeInTheDocument();
+});
+
+test("renders about", () => {
+  const { container, getByText } = render(<App />, {
+    initialEntries: ["/"],
+    initialIndex: 0,
+  });
+
+  fireEvent.click(getByText(/About/i));
+
+  const { getByText: sectionGetByText } = within(
+    container.querySelector(".section")
+  );
+
+  const about = sectionGetByText(/About/i);
+
+  expect(about).toBeInTheDocument();
+});
+
+test("renders 404", () => {
+  const { container, getByText } = render(<App />, {
+    initialEntries: ["/"],
+    initialIndex: 0,
+  });
+
+  fireEvent.click(getByText(/Not Found/i));
+
+  const { getByText: sectionGetByText } = within(
+    container.querySelector(".section")
+  );
+
+  const notFound = sectionGetByText(/404 ::Not Found/i);
+
+  expect(notFound).toBeInTheDocument();
 });
